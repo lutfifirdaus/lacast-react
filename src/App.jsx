eb115112a9a4b39833d7e100a39cdf11e3505585
lutfifirdaus <lutdaus007@gmail.com>
2022-03-27 16:57:16 +0700

    initiate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCont] = useState(0);
+  function increment() {
+    setCont(count+1);
+  }
+  function decrement() {
+    setCont(count-1);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
+
+        <div>
+          <button onClick={decrement}>-</button>
+          <span>{count}</span>  
+          <button onClick={increment}>+</button>
+        </div>
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
